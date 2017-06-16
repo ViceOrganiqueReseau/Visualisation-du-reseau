aa6657e7707871f933c9dcaba9a2c22183de6947
a2xl08 <axel.correia@etu.emse.fr>
2017-06-16 17:43:59 +0200

    FEAT: Affichage des liens

--- js/importdata.js
+++ js/importdata.js
@@ -6,6 +6,8 @@ var affiliations;
 var nodes;
 var circles;
 var simulation;
+var firstick = 1;
+var NameToIndex;
 
 // Création du faux DOM
 // Il reçoit les éléments grphiques avec 
@@ -22,7 +24,15 @@ d3.csv("data/Affiliation.csv", function (data){
 	affiliations = data;
 	console.log(affiliations);
 	console.log(dataset);
-	
+
+	// On renseigne les forces
+	simulation = d3.forceSimulation()
+					.force("center", d3.forceCenter(width/2,height/2))
+					//.force("charge", d3.forceManyBody());
+
+	simulation.nodes(dataset).on("tick", ticked);
+
+	// Binding des data avec les noeuds
 	circles = CustomDOM.selectAll("custom.circle")
 				.data(dataset)
 				.enter()
@@ -30,14 +40,41 @@ d3.csv("data/Affiliation.csv", function (data){
 				.attr("class", "circle")
 				.attr("r", 5)
 
-	simulation = d3.forceSimulation()
-					.force("center", d3.forceCenter(width/2,height/2))
-					//.force("charge", d3.forceManyBody());
-
-	simulation.nodes(dataset).on("tick", ticked);
-
 	function ticked (){
+
 		ctx.clearRect(0,0,width,height);
+		// On remplie l'annuaire
+		// Dictionnaire inversé pour faciliter les liens
+		if (firstick){
+			var NestedData = d3.nest()
+							.key(function (d){return d.Name})
+							.rollup(function (v){return v[0].index})
+							.entries(dataset);
+			NameToIndex = {};
+			console.log(NestedData)
+			NestedData.forEach(function (d){
+				NameToIndex[d.key] = d.value;
+			})
+			
+			console.log(NameToIndex);
+			firstick=0;	
+		}
+
+		// Traçage des liens
+		ctx.strokeStyle = "red"
+		ctx.lineWidth = 1;
+		affiliations.forEach(function (d){
+			ctx.beginPath()
+			var beginindex = NameToIndex[d.source];
+			var endindex = NameToIndex[d.target];
+			ctx.moveTo(Math.round(dataset[beginindex].x), Math.round(dataset[beginindex].y));
+			ctx.lineTo(Math.round(dataset[endindex].x), Math.round(dataset[endindex].y));
+			ctx.closePath();
+			ctx.stroke();
+		});
+		console.log(ctx)
+
+		// Les cercles
 		ctx.beginPath();
 		circles.each(function (d){
 			ctx.moveTo(d.x, d.y);
@@ -45,6 +82,7 @@ d3.csv("data/Affiliation.csv", function (data){
 		})
 		ctx.fillStyle = "green"
 		ctx.fill();
+
 	}
 
 });
