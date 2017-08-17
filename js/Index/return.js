87d966499c621d147e3f22ff2dde14555c34304f
a2xl08 <axel.correia@etu.emse.fr>
2017-08-17 10:07:28 +0200

    Crédits

--- js/Index/return.js
+++ js/Index/return.js
@@ -1,3 +1,18 @@
+/* Ce script gère le rechargement de la page par l'utilisateur
+via F5 ainsi que le bouton #reloadbox qui recharge la page depuis
+ le début */
+
 window.onbeforeunload = function(){
   window.scrollTo(0,0);
 }
+
+// L'affichage des crédits
+var opener = document.getElementById("opencredits");
+var closer = document.getElementById("closecredits");
+var element = document.getElementById("credits")
+opener.addEventListener("click", function (){
+  element.style.display = "block";
+})
+closer.addEventListener("click", function (){
+  element.style.display = "none";
+})
--- js/Reseau/return.js
+++ js/Reseau/return.js
@@ -5,3 +5,14 @@ via F5 ainsi que le bouton #reloadbox qui recharge la page depuis
 window.onbeforeunload = function(){
   window.scrollTo(0,0);
 }
+
+// L'affichage des crédits
+var opener = document.getElementById("opencredits");
+var closer = document.getElementById("closecredits");
+var element = document.getElementById("credits")
+opener.addEventListener("click", function (){
+  element.style.display = "block";
+})
+closer.addEventListener("click", function (){
+  element.style.display = "none";
+})
